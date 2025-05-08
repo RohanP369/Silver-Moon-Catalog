c74445b2aba311da067faff28612dba8d2b210d4
RohanP369 <143544939+RohanP369@users.noreply.github.com>
2025-05-08 14:22:56 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-2xl">
+      Silver Moon Catalog Home Page
+    </main>
+  );
+}
